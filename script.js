49c73bad8cd22a2b78e21847d79e9d1a89daa1a1
Jasmine Nackash <jasmine.nackash@gmail.com>
2024-04-20 15:07:28 -0400

    getBrightness function fixed. Before removing uppercase

--- script.js
+++ script.js
@@ -2,14 +2,21 @@
 const globalTooltip = setTooltip();
 
 //color for passwords - all
-const colorScalePwAll = d3.scaleQuantize()
-.domain([0, 100])
-.range(d3.schemePuBuGn[9]);
+const sqrtScalePwAll = d3.scaleSqrt()
+.domain([1, 100]) //max domain is calculated and changed later!
+.range([1, 30]);
+
+const colorScalePwAll = d3.scaleSequential(d3.interpolateOrRd)
+.domain([1, 100]);
 
 // color for passwords - first letter
-const colorScalePwFirst = d3.scaleQuantize()
-.domain([0, 100])
-.range(d3.schemeGreens[9]);
+
+const sqrtScalePwFirst = d3.scaleSqrt()
+.domain([1, 100])
+.range([1, 30]);
+
+const colorScalePwFirst = d3.scaleSequential(d3.interpolateOrRd)
+.domain([1, 100]);
 
 
 function countCharacters(wordList, countingFunction){
@@ -206,25 +213,37 @@ function colorKeysByOccurrence(charCounts, kbContainer, colorScale) {
     const color1 = colorScale(count1);
     const color2 = colorScale(count2);
 
+    console.log("Color 1: ", color1, " Color 2: ", color2);
+
     char1Div.style.backgroundColor = color1;
     char2Div.style.backgroundColor = color2;
 
-    char1Div.style.color = getBrightness(color1) > 127 ? 'black' : 'white';
-    char2Div.style.color = getBrightness(color2) > 127 ? 'black' : 'white';
+    char1Div.style.color = getBrightness(color1);
+    char2Div.style.color = getBrightness(color2);
+
+
   });
 }
 
 
 function getBrightness(color) {
-
-  const r = parseInt(color.substr(1, 2), 16);
-  const g = parseInt(color.substr(3, 2), 16);
-  const b = parseInt(color.substr(5, 2), 16);
-
+  // Extracting RGB values from the 'rgb(255, 255, 255)' format
+  const rgb = color.match(/\d+/g);  // Correctly extracts the RGB numbers as strings
+  
+  if (!rgb || rgb.length < 3) {
+      console.error('Invalid RGB format', color);
+      return 'black';  // Return black as a default in case of error
+  }
+  
+  const r = parseInt(rgb[0], 10);  // Extracts the red component
+  const g = parseInt(rgb[1], 10);  // Extracts the green component
+  const b = parseInt(rgb[2], 10);  // Extracts the blue component
+  
+  // Brightness calculation using the luminance formula
   const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-
-  // console.log(brightness);
-  return brightness;
-
+  
+  // Return 'black' if background is light; 'white' if dark
+  return brightness > 127 ? 'black' : 'white';
 }
+
 
